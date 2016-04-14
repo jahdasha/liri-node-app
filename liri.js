c3ac1d2398defe99c697f38f45a7116d831b5d7b
Jahdasha Flagg <jahdashaflagg@Jahdashas-MacBook-Pro.local>
2016-04-14 19:27:51 -0400

    start of hw

---
+++ liri.js
@@ -0,0 +1,9 @@
+// 2. At the top of the liri.js file make it so you grab the data from keys.js and store it into a variable to use
+// 3. Make it so liri.js can take in one of the following arguments
+//  * my-tweets
+//  * spotify-this-song
+//  * movie-this
+//  * do-what-it-says
+
+var fs = require("fs");
+var request = require("request");
