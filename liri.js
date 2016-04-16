659c95bbdffbf50b5ee7fc5114bda9a68d0e9a63
Jahdasha Flagg <jahdashaflagg@Jahdashas-MBP.home>
2016-04-16 08:13:54 -0400

    added to functions default search if nothing is entered & printed intrustions

--- liri.js
+++ liri.js
@@ -10,7 +10,6 @@
 	var keys = require("./keys.js");
 	var twitter = require("twitter");
 	var spotify = require ("spotify");
-
 	var liriArgument = process.argv[2];
 
 	switch(liriArgument) {
@@ -19,18 +18,20 @@
 		case "movie-this": movieThis(); break;
 		case "do-what-it-says": doWhatItSays(); break;
 		default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
-			"my-tweets <any twitter name>, " +"\r\n"+
-			"spotify-this-song <any song name>, "+"\r\n"+
-			"movie-this <any movie name>, "+"\r\n"+
-			"or do-what-it-says");
+			"1. my-tweets 'any twitter name' " +"\r\n"+
+			"2. spotify-this-song 'any song name' "+"\r\n"+
+			"3. movie-this 'any movie name' "+"\r\n"+
+			"4. do-what-it-says."+"\r\n"+
+			"Be sure to put the movie or song name in quotation marks if it's more than one word.");
 	};
 
 	function movieThis(){
 		var movie = process.argv[3];
-		if (!movie) {
-			movie = "mr nobody"
-		};
-		request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+		if(!movie){
+			movie = "mr nobody";
+		}
+		params = movie
+		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var movieObject = JSON.parse(body);
 				//console.log(movieObject); // Show the text in the terminal
@@ -43,7 +44,8 @@
 				"Plot: " + movieObject.Plot+"\r\n"+
 				"Actors: " + movieObject.Actors+"\r\n"+
 				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
-				"Rotten Tomatoes URL: " + movieObject.tomatoURL;
+				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+				"------------------------------ fin ------------------------------" + "\r\n";
 				console.log(movieResults)
 			} else {
 				console.log("Error :"+ error);
@@ -58,8 +60,10 @@
 			access_token_key: keys.twitterKeys.access_token_key,
 			access_token_secret: keys.twitterKeys.access_token_secret, 
 		});
-		var twitterUsername = "JahdashaFlagg";
-		twitterUsername = process.argv[3];
+		var twitterUsername = process.argv[3];
+		if(!twitterUsername){
+			twitterUsername = "JahdashaFlagg";
+		}
 		params = {screen_name: twitterUsername};
 		client.get("statuses/user_timeline/", params, function(error, data, response){
 			if (!error) {
@@ -79,9 +83,11 @@
 		});
 	}
 	function spotifyThisSong(songName) {
-		var songName = "What's my age again";
-		songName = process.argv[3];
-		params = {screen_name: songName};
+		var songName = process.argv[3];
+		if(!songName){
+			songName = "What's my age again";
+		}
+		params = songName;
 		spotify.search({ type: "track", query: params }, function(err, data) {
 			if(!err){
 				var songInfo = data.tracks.items;
